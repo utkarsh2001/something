51d752c883c92390e8966d3194a51441eabf0072
utkarsh2001 <154227507+utkarsh2001@users.noreply.github.com>
2023-12-22 15:50:20 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,29 @@
+/* scan andprint
+#include <stdio.h>
+int main(){
+	int b[5];
+	b[0]=64;
+	for(int f=0;f<=5;f++){
+	scanf("%d",&b[f]);
+	}
+	for(int f=0;f<=5;f++){
+	printf("%d",b[f]);
+	}
+	return 0;	
+}
+*/
+
+
+
+
+#include <stdio.h>
+int main(){
+	int b[]={12,34,57,43,23};
+	book(b,5);
+	return 0;	
+}
+
+
+int  book(int []){
+	print("%d",b[1]);
+}
